0798b1c1c0f3268b0c7d48fe5a2be7729a24c54c
mjhotham <matthotham@gmail.com>
2020-02-08 21:44:17 -0600

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode;
+
+class RobotConstants {
+
+   // This file contains most of the constants that may need to be adjusted between now and the competition.
+
+   // All target values are in inches
+
+
+    // For Auto and TeleOP
+
+    public static double TapeExtendPower = .8;
+
+    public static double TapeRetractPower = .2;
+
+
+    // For TELEOP
+
+
+  public static double WristCollectionPosition = .11;            // Dpad_Up
+  public static double WristFrontDepositPosition = .787;         // Y
+  public static double WristRightDepositPosition = .43;          // X
+    
+  public static double LeftHookDisengaged = .42;
+  public static double LeftHookEngaged = .12;
+
+  public static double RightHookDisengaged = .32;
+  public static double RightHookEngaged = .62;
+
+  public static double LeftAngleOpen = 0.3;
+  public static double LeftAngleIntake = 0.353;
+  public static double LeftAngleGripped = 0.42;
+
+  public static double RightAngleOpen = 0.58;
+  public static double RightAngleIntake = 0.52;
+  public static double RightAngleGripped = 0.45;
+
+  public static double GripperOpen = 1;
+  public static double GripperClosed = .64;
+
+  public static double WristOverRideSpeed = .002;
+
+  public static double YTopSlideTarget = 15.6;                  // Y
+  public static double XTopSlideTarget = 15.6;                  // X
+
+  public static double TopSlideCapstonePickupPosition = 10.5;
+
+  public static double OutTakePower = -1;                        // may need to change this to avoid launching penalties
+
+
+
+    // For BOTH Autos
+
+
+  public static double FirstDepositTopSlideTarget = 16;         // make sure the first stone is completely on the foundation so the robot doesn't push it when it comes back with the second stone
+  public static double SecondDepositTopSlideTarget = 14;
+
+  public static double FirstDepositHeight = 7;
+  public static double SecondDepositHeight = 12;                // lower will cause the stone to bounce less but could collide with first stone
+
+
+
+         // these are unlikely to be different from the TeleOp values but they are here just in case they need adjustment specific to auto
+
+  public static double GripperOpenAuto = 1;
+  public static double GripperClosedAuto = .64;
+
+  public static double WristCollectionPositionAuto = .11;           
+  public static double WristDepositPositionAuto = .767;         
+    
+  public static double LeftHookDisengagedAuto = .42;
+  public static double LeftHookEngagedAuto = .12;
+
+  public static double RightHookDisengagedAuto = .32;
+  public static double RightHookEngagedAuto = .62;
+
+  public static double LeftAngleGrippedAuto = 0.42;
+  public static double LeftAngleOpenAuto = 0.3;
+
+  public static double RightAngleOpenAuto = 0.58;
+  public static double RightAngleGrippedAuto = 0.45;
+
+
+
+    // For RED Auto
+
+
+  public static double LeftAngleScanningRed = .28;      // used in init to aim the camera at the 3 outer stones
+
+  // would only change these to improve the intaking of the skystones
+
+  public static double LeftAngleIntakeRed = 0.353;
+  public static double RightAngleIntakeRed = 0.52;
+
+
+
+    // For Blue Auto
+
+
+  public static double LeftAngleScanningBlue = .27;    // used in init to aim the camera at the 3 outer stones
+
+  // would only change these to improve the intaking of the skystones
+
+  public static double LeftAngleIntakeBlue = 0.353;
+  public static double RightAngleIntakeBlue = 0.52;
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/notSure.java
@@ -0,0 +1,396 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.drive.mecanum.SampleMecanumDriveREVOptimized;
+import org.openftc.revextensions2.RevBulkData;
+
+@TeleOp(name="MasterTeleOp")
+public class notSure extends LinearOpMode {
+
+    SampleMecanumDriveREVOptimized drive;
+    LiftManager lift;
+
+
+    void openIntake() {
+        drive.LeftIntake.setPower(0);
+        drive.RightIntake.setPower(0);
+        drive.Gripper.setPosition(RobotConstants.GripperOpen);
+        drive.LeftAngle.setPosition(RobotConstants.LeftAngleIntake);
+        drive.RightAngle.setPosition(RobotConstants.RightAngleIntake);
+    }
+
+    void turnOnIntake() {
+        drive.LeftIntake.setPower(1);
+        drive.RightIntake.setPower(1);
+        drive.Gripper.setPosition(RobotConstants.GripperOpen);
+        drive.LeftAngle.setPosition(RobotConstants.LeftAngleIntake);
+        drive.RightAngle.setPosition(RobotConstants.RightAngleIntake);
+    }
+
+    void gripIntake() {
+        drive.LeftIntake.setPower(0);
+        drive.RightIntake.setPower(0);
+        drive.Gripper.setPosition(RobotConstants.GripperClosed);
+        drive.LeftAngle.setPosition(RobotConstants.LeftAngleGripped);
+        drive.RightAngle.setPosition(RobotConstants.RightAngleGripped);
+    }
+
+    void ungripWheels() {
+        drive.LeftIntake.setPower(0);
+        drive.RightIntake.setPower(0);
+        drive.Gripper.setPosition(RobotConstants.GripperClosed);
+        drive.LeftAngle.setPosition(RobotConstants.LeftAngleOpen);
+        drive.RightAngle.setPosition(RobotConstants.RightAngleOpen);
+    }
+
+    void ejectIntake() {
+        drive.Gripper.setPosition(RobotConstants.GripperOpen);
+        drive.LeftAngle.setPosition(RobotConstants.LeftAngleIntake);
+        drive.RightAngle.setPosition(RobotConstants.RightAngleIntake);
+    }
+
+    void intakeState(int state) {
+        switch (state) {
+            case -1:
+                ejectIntake();
+                if (intakeState > 0) {
+                    ejectRequest = true;
+                    ejectTimer.reset();
+                } else {
+                    drive.LeftIntake.setPower(RobotConstants.OutTakePower);
+                    drive.RightIntake.setPower(RobotConstants.OutTakePower);
+                }
+                break;
+            case 0:
+                openIntake();
+                break;
+            case 1:
+                turnOnIntake();
+                break;
+            case 2:
+                gripIntake();
+                break;
+            case 3:
+                ungripWheels();
+                break;
+        }
+        intakeState = state;
+    }
+
+    int intakeState = 0;
+
+    boolean previousRightBumper = false;
+    boolean previousLeftBumper = false;
+    boolean PreviousGamePad1B = false;
+
+    boolean slideAtZero = false;
+
+    double forward, right, spin;
+    boolean previousLeftStickButton = false, previousRightStickButton = false, slowMode = false, reverseDrivetrain = false;
+    boolean previousDpadUp = false, hooksEngaged = false;
+
+    double wristRequestedPosition = -1;
+    boolean wristCollectionRequest = false;
+    int haveSomeGarbage = 0;
+
+    ElapsedTime ejectTimer = new ElapsedTime();
+
+    boolean ejectRequest = false;
+
+    Servo CapStoneLift;
+
+    ElapsedTime garbage = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new SampleMecanumDriveREVOptimized(hardwareMap);
+        lift = new LiftManager(drive.LeftLift, drive.RightLift, drive.Elbow, drive.LeftIntake);
+        intakeState(intakeState);
+
+        DigitalChannel zeroSwitch = hardwareMap.digitalChannel.get("ZeroSwitch");
+        zeroSwitch.setMode(DigitalChannel.Mode.INPUT);
+
+        telemetry.addData("intakeState", () -> intakeState);
+        telemetry.addData("lift.slideTargetIN", () -> lift.slideTargetIN);
+        telemetry.addData("lift.liftTargetIN", () -> lift.liftTargetIN);
+        telemetry.addData("lift.LiftPositionIN", () -> lift.LiftPositionIN);
+        telemetry.addData("lift.SlidePositionIN", () -> lift.SlidePositionIN);
+        telemetry.addData("lift.slideObstruction", () -> lift.slideObstruction);
+        telemetry.addData("lift.liftObstruction", () -> lift.liftObstruction);
+        telemetry.addData("wristCollectionRequest", () -> wristCollectionRequest);
+        telemetry.addData("wristRequestedPosition", () -> wristRequestedPosition);
+//        telemetry.addData("RightAngePosition", () -> drive.RightAngle.getPosition());
+//        telemetry.addData("LeftAnglePosition", () -> drive.LeftAngle.getPosition());
+
+        CapStoneLift = hardwareMap.get(Servo.class, "CapStoneLift");
+
+        waitForStart();
+        while (opModeIsActive()) {
+            RevBulkData bulkData2 = drive.hub2.getBulkInputData();
+
+            if (intakeState != -1) {
+                ejectRequest = false;
+            }
+
+            if (ejectRequest && ejectTimer.seconds() > .4) {
+                ejectRequest = false;
+                drive.LeftIntake.setPower(RobotConstants.OutTakePower);
+                drive.RightIntake.setPower(RobotConstants.OutTakePower);
+            }
+
+            double triggerSum = gamepad1.right_trigger - gamepad1.left_trigger;
+            if (triggerSum > 0.1 && intakeState == 2) {
+                intakeState = 3;
+                intakeState(intakeState);
+            }
+
+            if (gamepad2.right_bumper) {
+                drive.Elbow.setPosition(Range.scale(gamepad2.left_stick_y, -1, 1, .2, .8));
+                lift.update(bulkData2, triggerSum, gamepad1.back, true);
+            } else
+                lift.update(bulkData2, triggerSum, gamepad1.back, false);
+
+            if (lift.slideTargetIN < 3 && lift.liftTargetIN < 6) {//code only active while slide and lift really close
+
+                if (gamepad1.right_bumper) {
+                    if (!previousRightBumper) {
+                        intakeState = intakeState == 1 ? 2 : 1;//intake or wheel gripped
+                        drive.Wrist.setPosition(RobotConstants.WristCollectionPosition);
+                        intakeState(intakeState);
+                    }
+                    previousRightBumper = true;
+                } else {
+                    previousRightBumper = false;
+                }
+
+                if (gamepad1.left_bumper) {
+                    if (!previousLeftBumper) {
+                        intakeState(intakeState == -1 ? 0 : -1);//open or outake
+                    }
+                    previousLeftBumper = true;
+                } else {
+                    previousLeftBumper = false;
+                }
+
+            } else {
+
+                if (gamepad1.left_bumper) {
+                    if (!previousLeftBumper) {
+                        previousLeftBumper = true;
+                        drive.LeftIntake.setPower(RobotConstants.OutTakePower);
+                        drive.RightIntake.setPower(RobotConstants.OutTakePower);
+                    }
+                } else if (previousLeftBumper) {
+                    previousLeftBumper = false;
+                    drive.LeftIntake.setPower(0);
+                    drive.RightIntake.setPower(0);
+                }
+
+                if (gamepad1.right_bumper) {
+                    if (!previousRightBumper) {
+                        intakeState = intakeState == 3 ? 0 : 3;
+                        intakeState(intakeState);
+                    }
+                    previousRightBumper = true;
+                } else {
+                    previousRightBumper = false;
+                }
+            }
+
+            if (gamepad1.dpad_down) {
+                intakeState = 0;
+                intakeState(intakeState);
+                lift.slideTargetIN = 0;
+                lift.liftTargetIN = 0;
+                wristCollectionRequest = true;
+            }
+
+            if (gamepad1.x) {
+                wristRequestedPosition = RobotConstants.WristRightDepositPosition;
+                lift.slideTargetIN = RobotConstants.XTopSlideTarget;
+                if (lift.liftTargetIN < 8) {
+                    lift.liftTargetIN = 8;
+                }
+                drive.Gripper.setPosition(RobotConstants.GripperClosed);
+                intakeState = 3;
+                intakeState(intakeState);
+            }
+
+            if (gamepad1.y) {
+                wristRequestedPosition = RobotConstants.WristFrontDepositPosition;
+                lift.slideTargetIN = RobotConstants.YTopSlideTarget;
+                if (lift.liftTargetIN < 8) {
+                    lift.liftTargetIN = 8;
+                }
+                drive.Gripper.setPosition(RobotConstants.GripperClosed);
+                intakeState = 3;
+                intakeState(intakeState);
+            }
+
+            if (gamepad2.dpad_down) {
+                haveSomeGarbage = 1;
+                intakeState(3);
+            }
+
+            switch (haveSomeGarbage) {
+                case 1:
+                    wristRequestedPosition = RobotConstants.WristRightDepositPosition;
+                    CapStoneLift.setPosition(0.2);
+                    haveSomeGarbage++;
+                    garbage.reset();
+                    lift.liftTargetIN = 7;
+                    break;
+                case 2:
+                    if (garbage.seconds() > 1) {
+                        haveSomeGarbage++;
+                        CapStoneLift.setPosition(0.5);
+                        wristRequestedPosition = RobotConstants.WristRightDepositPosition;
+                        lift.slideTargetIN = 14;
+                    }
+                    break;
+                case 3:
+                    if (wristRequestedPosition < 0) {
+                        haveSomeGarbage++;
+                        lift.slideTargetIN = RobotConstants.TopSlideCapstonePickupPosition;
+                    }
+                    break;
+                default:
+                    haveSomeGarbage = 0;
+            }
+
+            if (gamepad1.b) {                 // Toggle Gripper no matter the superstructures state even though gamepad1.right_bumper is probably sufficient because of the conditions
+                if (!PreviousGamePad1B) {
+
+                    if (drive.Gripper.getPosition() != RobotConstants.GripperOpen) {
+                        drive.Gripper.setPosition(RobotConstants.GripperOpen);
+
+                    } else if (drive.Gripper.getPosition() != RobotConstants.GripperClosed) {
+                        drive.Gripper.setPosition(RobotConstants.GripperClosed);
+                    }
+
+                    PreviousGamePad1B = true;
+                }
+            } else
+                PreviousGamePad1B = false;
+
+
+            //automatic wrist position stuff code I guess
+            if (wristRequestedPosition != -1 && lift.SlidePositionIN > 12) {   //only use for positions requiring extension
+                drive.Wrist.setPosition(wristRequestedPosition);
+                wristRequestedPosition = -1;
+            } else if (wristCollectionRequest && lift.SlidePositionIN < 12) {   //only use for going to collection position
+                drive.Wrist.setPosition(RobotConstants.WristCollectionPosition);
+                wristCollectionRequest = false;
+            }
+
+            //toggle hooks
+            if (gamepad1.dpad_up) {
+                if (!previousDpadUp) {
+                    previousDpadUp = true;
+                    hooksEngaged = !hooksEngaged;
+                    drive.LeftHook.setPosition(hooksEngaged ? RobotConstants.LeftHookEngaged : RobotConstants.LeftHookDisengaged);
+                    drive.RightHook.setPosition(hooksEngaged ? RobotConstants.RightHookEngaged : RobotConstants.RightHookDisengaged);
+                }
+            } else
+                previousDpadUp = false;
+
+
+            //does anyone even use slow mode?
+            if (gamepad1.left_stick_button) {
+                if (!previousLeftStickButton) {
+                    previousLeftStickButton = true;
+                    slowMode = !slowMode;
+                }
+            } else
+                previousLeftStickButton = false;
+
+
+            //hopefully no one uses the invert drivetrain button
+            if (gamepad1.right_stick_button) {
+                if (!previousRightStickButton) {
+                    previousRightStickButton = true;
+                    reverseDrivetrain = !reverseDrivetrain;
+                }
+            } else
+                previousRightStickButton = false;
+
+            forward = gamepad1.left_stick_y * (reverseDrivetrain ? 1 : -1) * (slowMode ? 0.3 : 1);
+            right = gamepad1.left_stick_x * (reverseDrivetrain ? -1 : 1) * (slowMode ? 0.3 : 1);
+            spin = gamepad1.right_stick_x * (slowMode ? 0.4 : 1);
+
+            double maxwheel = Math.abs(forward) + Math.abs(right) + Math.abs(spin);
+            if (maxwheel > 1) {
+                forward /= maxwheel;
+                right /= maxwheel;
+                spin /= maxwheel;
+            }
+
+            drive.setMotorPowers(forward + spin + right, forward + spin - right, forward - spin + right, forward - spin - right);
+
+            //top slide encoder reset code
+            boolean slideSensor = !bulkData2.getDigitalInputState(zeroSwitch);     //this is reversed, deal with it
+            if (!slideAtZero && slideSensor) {
+                drive.Elbow.setPosition(0.5);
+                //intake wheel doesn't need encoder so it's used for the top slide encoder
+                drive.LeftIntake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                drive.LeftIntake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                intakeState(intakeState);
+                slideAtZero = true;
+            }
+            //wait until top slide moves more than 4 inches out before it can trigger the sensor again
+            if (slideAtZero && lift.SlidePositionIN > 4 && !slideSensor)
+                slideAtZero = false;
+            telemetry.addData("slideAtZero", slideAtZero);
+
+            //add manual override for top slide in case if automation fails or small adjustment is needed
+            if (gamepad2.left_bumper) {
+                CapStoneLift.setPosition(Range.clip(Range.scale(gamepad2.right_stick_y, -1.0, 1.0, .2, .8), .35, .8));
+            } else if (haveSomeGarbage == 0) {
+                CapStoneLift.setPosition(.5);
+            }
+
+            if (gamepad2.x) {
+                drive.Wrist.setPosition(drive.Wrist.getPosition() + RobotConstants.WristOverRideSpeed);
+            }
+
+            if (gamepad2.b) {
+                drive.Wrist.setPosition(drive.Wrist.getPosition() - RobotConstants.WristOverRideSpeed);
+            }
+
+            drive.Tape.setPosition(Range.scale((gamepad2.right_trigger - gamepad2.left_trigger),-1,1,.2,.8));
+
+
+              // for diagnostic purposes
+
+//
+//            if (gamepad2.y) {
+//                drive.RightAngle.setPosition(drive.RightAngle.getPosition() + RobotConstants.WristOverRideSpeed);
+//            }
+//
+//            if (gamepad2.a) {
+//                drive.RightAngle.setPosition(drive.RightAngle.getPosition() - RobotConstants.WristOverRideSpeed);
+//            }
+//
+//            if (gamepad2.right_stick_button) {
+//                drive.LeftAngle.setPosition(drive.LeftAngle.getPosition() - RobotConstants.WristOverRideSpeed);
+//            }
+//
+//            if (gamepad2.left_stick_button) {
+//                drive.LeftAngle.setPosition(drive.LeftAngle.getPosition() + RobotConstants.WristOverRideSpeed);
+//            }
+//
+
+
+            telemetry.update();
+
+        }
+    }
+}
